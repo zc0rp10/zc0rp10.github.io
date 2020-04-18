48607be8833bf8058f547b7eafad294144780481
zc0rp10 <zcorpio@gmail.com>
2020-04-18 15:10:53 +0200

    hyrax gen

---
+++ portfolio/product-landing-page/main.js
@@ -0,0 +1,44 @@
+const $ = document.querySelector.bind(document);
+
+const hbBtn = $(".hb-btn");
+const cartBtn = $(".cart-btn");
+const siteNav = $(".site-nav");
+
+cartBtn.addEventListener("click", () => console.log("cart"));
+
+hbBtn.addEventListener("click", () => siteNav.classList.toggle("open"));
+
+
+
+// var slideIndex = 1;
+// showSlides(slideIndex);
+
+// // Next/previous controls
+// function plusSlides(n) {
+//   showSlides((slideIndex += n));
+// }
+
+// // Thumbnail image controls
+// function currentSlide(n) {
+//   showSlides((slideIndex = n));
+// }
+
+// function showSlides(n) {
+//   var i;
+//   var slides = document.getElementsByClassName("mySlides");
+//   var dots = document.getElementsByClassName("dot");
+//   if (n > slides.length) {
+//     slideIndex = 1;
+//   }
+//   if (n < 1) {
+//     slideIndex = slides.length;
+//   }
+//   for (i = 0; i < slides.length; i++) {
+//     slides[i].style.display = "none";
+//   }
+//   for (i = 0; i < dots.length; i++) {
+//     dots[i].className = dots[i].className.replace(" active", "");
+//   }
+//   slides[slideIndex - 1].style.display = "block";
+//   dots[slideIndex - 1].className += " active";
+// }
